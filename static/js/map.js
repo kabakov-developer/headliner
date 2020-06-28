933392d80f5ac7fd501f42d7899c0edf36708a5f
unknown <kabakov0107@gmail.com>
2020-06-28 21:32:50 +0600

    add past site

--- static/js/map.js
+++ static/js/map.js
@@ -1,37 +1,37 @@
 $(document).ready(function(){
 
 
-ymaps.ready(init);
-
-function init() {
-    var myMap = new ymaps.Map("map", {
-            center: [43.240224, 76.893480],
-            zoom: 15,
-			controls: []
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [55.751574, 37.573856],
+            zoom: 14,
+            controls: []
         }, {
             searchControlProvider: 'yandex#search'
         }),
 
-    // Creating a geo object with the "Point" geometry type.
-        myGeoObject = new ymaps.GeoObject({
-            // The geometry description.
-            geometry: {
-                type: "Point",
-                coordinates: [43.240224, 76.893480]
-            },
-        }, {
-            // The placemark can be dragged.
-            draggable: true
-        });
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
 
-    myMap.geoObjects
-        .add(myGeoObject)
-        // .add(myPieChart)
-        .add(new ymaps.Placemark([43.240224, 76.893480], {
-            balloonContent: 'the color of <strong>the water on Bondi Beach</strong>'
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
         }, {
-            preset: 'islands#icon',
-            iconColor: '#062c95'
-        }))
-}
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: '/img/icons/map-icon.png',
+            // Размеры метки.
+            iconImageSize: [44, 50],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        });
+    myMap.geoObjects
+        .add(myPlacemark)
+        .add(myPlacemarkWithContent);
+});
+
+
 })
