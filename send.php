354b3c90bea98cf43e9eb92e6e0fd7925f97cfa9
unknown <kabakov0107@gmail.com>
2020-07-06 16:12:48 +0600

    add full site

--- send.php
+++ send.php
@@ -1,14 +1,13 @@
 <?php
 	if(isset($_POST['submit'])){
 	/* Устанавливаем e-mail Кому и от Кого будут приходить письма */
-	$to = "kabakov0107@gmail.com"; // Здесь нужно написать e-mail, куда будут приходить письма
-	$from = "admin@hardworkers.kz"; // Здесь нужно написать e-mail, от кого будут приходить письма, например no-reply(собака)epicblog.net
+	$to = "Estatesochi@mail.ru"; // Здесь нужно написать e-mail, куда будут приходить письма
+	$from = "admin@headliner.kz"; // Здесь нужно написать e-mail, от кого будут приходить письма, например no-reply(собака)epicblog.net
 
 	/* Указываем переменные, в которые будет записываться информация с формы */
 	$first_name = $_POST['first_name'];
 	$phone = $_POST['phone'];
-	$email = $_POST['email'];
-	$subject = "Форма отправки сообщений с сайта kabakov.kz";
+	$subject = "Форма отправки сообщений с сайта http://estategroup-msk.ru/.";
 
 	/* Проверка правильного написания e-mail адреса */
 	// if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email))
@@ -21,7 +20,6 @@
 		Было отправлена заявка с сайта!
 		Имя: $first_name
 		Номер телефона: $phone
-		E-mail: $email
 	";
 
 	$headers = "From: $from \r\n";
@@ -29,6 +27,6 @@
 	/* Отправка сообщения, с помощью функции mail() */
 	mail($to, $subject, $mail_to_myemail, $headers . 'Content-type: text/plain; charset=utf-8');
 	echo "Сообщение отправлено. Спасибо Вам " . $first_name . ", мы скоро свяжемся с Вами.";
-	echo "<br /><br /><a href='https://kabakov.kz'>Вернуться на сайт.</a>";
+	echo "<br /><br /><a href='http://estategroup-msk.ru/'>Вернуться на сайт.</a>";
 	}
 ?>
